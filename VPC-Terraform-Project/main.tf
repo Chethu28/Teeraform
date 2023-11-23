19a4ffbb6ee413cd789d7c4156875dd292078b5b
Chethan Reddy <95994938+Chethu28@users.noreply.github.com>
2023-11-23 22:29:24 +0530

    Update main.tf

--- VPC-Terraform-Project/main.tf
+++ VPC-Terraform-Project/main.tf
@@ -38,7 +38,7 @@ resource "aws_route_table" "myroutetables" {
         Name = "myroutetables"
     }
     route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/24"
     gateway_id = aws_internet_gateway.igw.id
   }
 }
